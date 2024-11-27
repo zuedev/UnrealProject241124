aa69a4e5b5fbb2e364b85e87c2ec93a0147d8b31
Alex Pooley <zuedev@gmail.com>
2024-11-27 11:27:34 +0000

    rev to blank template

---
+++ Source/UnrealProject241124/UnrealProject241124.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealProject241124.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealProject241124, "UnrealProject241124" );
