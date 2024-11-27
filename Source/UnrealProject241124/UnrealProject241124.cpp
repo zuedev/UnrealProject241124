2dc8e1d64e31d9e15e963cb16d1543a4b259ec07
Alex Pooley <zuedev@gmail.com>
2024-11-27 11:19:00 +0000

    fps template

--- Source/UnrealProject241124/UnrealProject241124.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealProject241124.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealProject241124, "UnrealProject241124" );
